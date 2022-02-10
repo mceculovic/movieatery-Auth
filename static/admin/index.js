3511f05449180106ebd18a07258cc762f588227c
Milos Ceculovic <mceculovic2819rn@raf.rs>
2022-02-10 01:34:01 +0100

    committing

--- static/admin/index.js
+++
@@ -1,9 +0,0 @@
-
-function init(){
-
-    document.getElementById("logout").addEventListener("click", e => {
-        document.cookie = `token=;SameSite=Lax;path=/admin`;
-        console.log("SET");
-        window.location.href = '/admin/login';
-    });
-}
